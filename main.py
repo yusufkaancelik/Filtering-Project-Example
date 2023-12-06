3ab4863fa4c5db0a49045f2d4de333a705d39095
Yusuf Kaan Çelik <84715164+yusufkaancelik@users.noreply.github.com>
2023-12-06 23:20:01 +0300

    main.py

--- main.py
+++ main.py
@@ -1,487 +1,487 @@
 from tkinter import *
 iller = {
 "Adana" : {
-    "Teknoloji": "3",
-         "Moda": "2",
-     "Aksesuar": "1",
-        "Emlak": "4"},
+   "Technology": "3",
+      "Fashion": "2",
+    "Accessory": "1",
+  "Real Estate": "4"},
 
 "Adıyaman" : {
-    "Teknoloji": "0",
-         "Moda": "3",
-     "Aksesuar": "4",
-        "Emlak": "5"},
+   "Technology": "0",
+      "Fashion": "3",
+    "Accessory": "4",
+  "Real Estate": "5"},
 
 "Afyonkarahisar" : {
-    "Teknoloji": "1",
-         "Moda": "4",
-     "Aksesuar": "3",
-        "Emlak": "3"},
+   "Technology": "1",
+      "Fashion": "4",
+    "Accessory": "3",
+  "Real Estate": "3"},
 
 "Ağrı" : {
-    "Teknoloji": "1",
-         "Moda": "8",
-     "Aksesuar": "2",
-        "Emlak": "1"},
+   "Technology": "1",
+      "Fashion": "8",
+    "Accessory": "2",
+  "Real Estate": "1"},
 
 "Aksaray" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "2"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "2"},
 
 "Amasya" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "0",
-        "Emlak": "4"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "0",
+  "Real Estate": "4"},
 
 "Ankara" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "2",
-        "Emlak": "9"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "2",
+  "Real Estate": "9"},
 
 "Antalya" : {
-    "Teknoloji": "2",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "1"},
+   "Technology": "2",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "1"},
 
 "Ardahan" : {
-    "Teknoloji": "1",
-         "Moda": "0",
-     "Aksesuar": "2",
-        "Emlak": "6"},
+   "Technology": "1",
+      "Fashion": "0",
+    "Accessory": "2",
+  "Real Estate": "6"},
 
 "Artvin" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "0",
-        "Emlak": "2"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "0",
+  "Real Estate": "2"},
 
 "Aydın" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "3",
-        "Emlak": "3"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "3",
+  "Real Estate": "3"},
 
 "Balıkesir" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "6"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "6"},
 
 "Bartın" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "1"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "1"},
 
 "Batman" : {
-    "Teknoloji": "2",
-         "Moda": "0",
-     "Aksesuar": "1",
-        "Emlak": "3"},
+   "Technology": "2",
+      "Fashion": "0",
+    "Accessory": "1",
+  "Real Estate": "3"},
 
 "Bayburt" : {
-    "Teknoloji": "1",
-         "Moda": "0",
-     "Aksesuar": "2",
-        "Emlak": "6"},
+   "Technology": "1",
+      "Fashion": "0",
+    "Accessory": "2",
+  "Real Estate": "6"},
 
 "Bilecik" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "2",
-        "Emlak": "4"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "2",
+  "Real Estate": "4"},
 
 "Bingöl" : {
-    "Teknoloji": "0",
-         "Moda": "3",
-     "Aksesuar": "4",
-        "Emlak": "5"},
+   "Technology": "0",
+      "Fashion": "3",
+    "Accessory": "4",
+  "Real Estate": "5"},
 
 "Bitlis" : {
-    "Teknoloji": "1",
-         "Moda": "4",
-     "Aksesuar": "3",
-        "Emlak": "3"},
+   "Technology": "1",
+      "Fashion": "4",
+    "Accessory": "3",
+  "Real Estate": "3"},
 
 "Bolu" : {
-    "Teknoloji": "1",
-         "Moda": "8",
-     "Aksesuar": "2",
-        "Emlak": "1"},
+   "Technology": "1",
+      "Fashion": "8",
+    "Accessory": "2",
+  "Real Estate": "1"},
 
 "Burdur" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "2"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "2"},
 
 "Bursa" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "0",
-        "Emlak": "4"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "0",
+  "Real Estate": "4"},
 
 "Çanakkale" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "2",
-        "Emlak": "9"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "2",
+  "Real Estate": "9"},
 
 "Çankırı" : {
-    "Teknoloji": "2",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "1"},
+   "Technology": "2",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "1"},
 
 "Çorum" : {
-    "Teknoloji": "1",
-         "Moda": "0",
-     "Aksesuar": "2",
-        "Emlak": "6"},
+   "Technology": "1",
+      "Fashion": "0",
+    "Accessory": "2",
+  "Real Estate": "6"},
 
 "Denizli" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "0",
-        "Emlak": "2"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "0",
+  "Real Estate": "2"},
 
 "Diyarbakır" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "3",
-        "Emlak": "3"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "3",
+  "Real Estate": "3"},
 
 "Düzce" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "6"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "6"},
 
 "Edirne" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "1"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "1"},
 
 "Elazığ" : {
-    "Teknoloji": "2",
-         "Moda": "0",
-     "Aksesuar": "1",
-        "Emlak": "3"},
+   "Technology": "2",
+      "Fashion": "0",
+    "Accessory": "1",
+  "Real Estate": "3"},
 
 "Erzincan" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "2",
-        "Emlak": "4"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "2",
+  "Real Estate": "4"},
 
 "Erzurum" : {
-    "Teknoloji": "0",
-         "Moda": "3",
-     "Aksesuar": "4",
-        "Emlak": "5"},
+   "Technology": "0",
+      "Fashion": "3",
+    "Accessory": "4",
+  "Real Estate": "5"},
 
 "Eskişehir" : {
-    "Teknoloji": "1",
-         "Moda": "4",
-     "Aksesuar": "3",
-        "Emlak": "3"},
+   "Technology": "1",
+      "Fashion": "4",
+    "Accessory": "3",
+  "Real Estate": "3"},
 
 "Gaziantep" : {
-    "Teknoloji": "1",
-         "Moda": "8",
-     "Aksesuar": "2",
-        "Emlak": "1"},
+   "Technology": "1",
+      "Fashion": "8",
+    "Accessory": "2",
+  "Real Estate": "1"},
 
 "Giresun" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "2"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "2"},
 
 "Gümüşhane" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "0",
-        "Emlak": "4"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "0",
+  "Real Estate": "4"},
 
 "Hakkari" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "2",
-        "Emlak": "9"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "2",
+  "Real Estate": "9"},
 
 "Hatay" : {
-    "Teknoloji": "2",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "1"},
+   "Technology": "2",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "1"},
 
 "Igdır" : {
-    "Teknoloji": "1",
-         "Moda": "0",
-     "Aksesuar": "2",
-        "Emlak": "6"},
+   "Technology": "1",
+      "Fashion": "0",
+    "Accessory": "2",
+  "Real Estate": "6"},
 
 "Isparta" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "0",
-        "Emlak": "2"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "0",
+  "Real Estate": "2"},
 
 "İstanbul" : {
-    "Teknoloji": "12"},
+   "Technology": "12"},
 
 "İzmir" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "6"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "6"},
 
 "Kahramanmaraş" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "1"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "1"},
 
 "Karabük" : {
-    "Teknoloji": "2",
-         "Moda": "0",
-     "Aksesuar": "1",
-        "Emlak": "3"},
+   "Technology": "2",
+      "Fashion": "0",
+    "Accessory": "1",
+  "Real Estate": "3"},
 
 "Karaman" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "2",
-        "Emlak": "4"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "2",
+  "Real Estate": "4"},
 
 "Kars" : {
-    "Teknoloji": "0",
-         "Moda": "3",
-     "Aksesuar": "4",
-        "Emlak": "5"},
+   "Technology": "0",
+      "Fashion": "3",
+    "Accessory": "4",
+  "Real Estate": "5"},
 
 "Kastamonu" : {
-    "Teknoloji": "1",
-         "Moda": "4",
-     "Aksesuar": "3",
-        "Emlak": "3"},
+   "Technology": "1",
+      "Fashion": "4",
+    "Accessory": "3",
+  "Real Estate": "3"},
 
 "Kayseri" : {
-    "Teknoloji": "1",
-         "Moda": "8",
-     "Aksesuar": "2",
-        "Emlak": "1"},
+   "Technology": "1",
+      "Fashion": "8",
+    "Accessory": "2",
+  "Real Estate": "1"},
 
 "Kırıkkale" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "2"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "2"},
 
 "Kırklareli" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "2"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "2"},
 
 "Kırşehir" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "0",
-        "Emlak": "4"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "0",
+  "Real Estate": "4"},
 
 "Kilis" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "2",
-        "Emlak": "9"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "2",
+  "Real Estate": "9"},
 
 "Kocaeli" : {
-    "Teknoloji": "2",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "1"},
+   "Technology": "2",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "1"},
 
 "Konya" : {
-    "Teknoloji": "1",
-         "Moda": "0",
-     "Aksesuar": "2",
-        "Emlak": "6"},
+   "Technology": "1",
+      "Fashion": "0",
+    "Accessory": "2",
+  "Real Estate": "6"},
 
 "Kütahya" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "0",
-        "Emlak": "2"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "0",
+  "Real Estate": "2"},
 
 "Malatya" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "3",
-        "Emlak": "3"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "3",
+  "Real Estate": "3"},
 
 "Manisa" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "6"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "6"},
 
 "Mardin" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "1"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "1"},
 
 "Mersin" : {
-    "Teknoloji": "2",
-         "Moda": "0",
-     "Aksesuar": "1",
-        "Emlak": "3"},
+   "Technology": "2",
+      "Fashion": "0",
+    "Accessory": "1",
+  "Real Estate": "3"},
 
 "Mugla" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "2",
-        "Emlak": "4"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "2",
+  "Real Estate": "4"},
 
 "Muş" : {
-    "Teknoloji": "0",
-         "Moda": "3",
-     "Aksesuar": "4",
-        "Emlak": "5"},
+   "Technology": "0",
+      "Fashion": "3",
+    "Accessory": "4",
+  "Real Estate": "5"},
 
 "Nevşehir" : {
-    "Teknoloji": "1",
-         "Moda": "4",
-     "Aksesuar": "3",
-        "Emlak": "3"},
+   "Technology": "1",
+      "Fashion": "4",
+    "Accessory": "3",
+  "Real Estate": "3"},
 
 "Niğde" : {
-    "Teknoloji": "1",
-         "Moda": "8",
-     "Aksesuar": "2",
-        "Emlak": "1"},
+   "Technology": "1",
+      "Fashion": "8",
+    "Accessory": "2",
+  "Real Estate": "1"},
 
 "Ordu" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "2"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "2"},
 
 "Osmaniye" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "0",
-        "Emlak": "4"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "0",
+  "Real Estate": "4"},
 
 "Rize" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "2",
-        "Emlak": "9"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "2",
+  "Real Estate": "9"},
 
 "Sakarya" : {
-    "Teknoloji": "2",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "1"},
+   "Technology": "2",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "1"},
 
 "Samsun" : {
-    "Teknoloji": "1",
-         "Moda": "0",
-     "Aksesuar": "2",
-        "Emlak": "6"},
+   "Technology": "1",
+      "Fashion": "0",
+    "Accessory": "2",
+  "Real Estate": "6"},
 
 "Siirt" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "0",
-        "Emlak": "2"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "0",
+  "Real Estate": "2"},
 
 "Sinop" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "3",
-        "Emlak": "3"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "3",
+  "Real Estate": "3"},
 
 "Sivas" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "6"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "6"},
 
 "Şanlıurfa" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "1"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "1"},
 
 "Şırnak" : {
-    "Teknoloji": "2",
-         "Moda": "0",
-     "Aksesuar": "1",
-        "Emlak": "3"},
+   "Technology": "2",
+      "Fashion": "0",
+    "Accessory": "1",
+  "Real Estate": "3"},
 
 "Tekirdağ" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "2",
-        "Emlak": "4"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "2",
+  "Real Estate": "4"},
 
 "Tokat" : {
-    "Teknoloji": "0",
-         "Moda": "3",
-     "Aksesuar": "4",
-        "Emlak": "5"},
+   "Technology": "0",
+      "Fashion": "3",
+    "Accessory": "4",
+  "Real Estate": "5"},
 
 "Trabzon" : {
-    "Teknoloji": "1",
-         "Moda": "4",
-     "Aksesuar": "3",
-        "Emlak": "3"},
+   "Technology": "1",
+      "Fashion": "4",
+    "Accessory": "3",
+  "Real Estate": "3"},
 
 "Tunceli" : {
-    "Teknoloji": "1",
-         "Moda": "8",
-     "Aksesuar": "2",
-        "Emlak": "1"},
+   "Technology": "1",
+      "Fashion": "8",
+    "Accessory": "2",
+  "Real Estate": "1"},
 
 "Uşak" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "2"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "2"},
 
 "Van" : {
-    "Teknoloji": "0",
-         "Moda": "1",
-     "Aksesuar": "0",
-        "Emlak": "4"},
+   "Technology": "0",
+      "Fashion": "1",
+    "Accessory": "0",
+  "Real Estate": "4"},
 
 "Yalova" : {
-    "Teknoloji": "1",
-         "Moda": "1",
-     "Aksesuar": "2",
-        "Emlak": "9"},
+   "Technology": "1",
+      "Fashion": "1",
+    "Accessory": "2",
+  "Real Estate": "9"},
 
 "Yozgat" : {
-    "Teknoloji": "2",
-         "Moda": "1",
-     "Aksesuar": "1",
-        "Emlak": "1"},
+   "Technology": "2",
+      "Fashion": "1",
+    "Accessory": "1",
+  "Real Estate": "1"},
 
 "Zonguldak" : {
-    "Teknoloji": "1",
-         "Moda": "0",
-     "Aksesuar": "2",
-        "Emlak": "6"}
+   "Technology": "1",
+      "Fashion": "0",
+    "Accessory": "2",
+  "Real Estate": "6"}
 }
 
 def gonder():
@@ -498,15 +498,15 @@ def gonder():
             veri_frame = Frame(bg, bg="#DB9C1A")
             veri_frame.place(relx=0, rely=0.515, relwidth=1, relheight=0.218)
 
-            metin = f"{secilen_sehir} \nAdetler: "
+            metin = f"{secilen_sehir}:\n"
             metin += ' '.join([f"{kategori}: {il_verileri[kategori]}\n" for kategori in secilen_kategoriler_isim])
 
             etiket = Label(veri_frame, text=metin, font="Gilroy-bold 16 bold", fg="black", bg="#DB9C1A")
             etiket.pack(padx=10, pady=10)
         else:
-            hata_mesaji.set("Geçersiz kategori seçimi.")
+            hata_mesaji.set("Invalid category selection.")
     else:
-        hata_mesaji.set("Geçersiz şehir seçimi.")
+        hata_mesaji.set("Invalid city selection.")
 
 master = Tk()
 
@@ -519,13 +519,13 @@ bg.place(relx=0,rely=0,relwidth=1,relheight=1)
 ust= Frame(master, bg="#DB5F1A")
 ust.place(relx=0,rely=0,relwidth=1,relheight=0.1)
 
-s_yazi= Label(ust,bg="#DB5F1A",text="Filtreleme Örnek Proje",font="Gilroy-bold 24 bold", pady=20)
+s_yazi= Label(ust,bg="#DB5F1A",text="Filtering Project Example",font="Gilroy-bold 24 bold", pady=20)
 s_yazi.pack()
 
 acilir_bg= Frame(master, bg="#DB9C1A")
 acilir_bg.place(relx=0,rely=0.101,relwidth=1,relheight=0.05)
 
-sehirsec_yazi= Label(acilir_bg,bg="#DB9C1A",text="Şehir seç :",font="Gilroy-bold 20 bold", fg="black")
+sehirsec_yazi= Label(acilir_bg,bg="#DB9C1A",text="Select City:",font="Gilroy-bold 20 bold", fg="black")
 sehirsec_yazi.pack(side= LEFT)
 
 acilir= StringVar(acilir_bg)
@@ -622,13 +622,13 @@ acilir_menu.pack(padx=10, pady=10, side=LEFT)
 radio_bg= Frame(master, bg="#DB9C1A")
 radio_bg.place(relx=0,rely=0.152,relwidth=1,relheight=0.3)
 
-radio_yazi=Label(radio_bg,bg="#DB9C1A",text="Kategori seç:",font="Gilroy-bold 20 bold", fg="black")
+radio_yazi=Label(radio_bg,bg="#DB9C1A",text="Select category:",font="Gilroy-bold 20 bold", fg="black")
 radio_yazi.pack(anchor=NW)
 
 var = [IntVar() for _ in range(4)]
 
 check_buttons = []
-kategori_isimleri = ["Teknoloji", "Moda", "Aksesuar", "Emlak"]
+kategori_isimleri = ["Technology", "Fashion", "Accessory", "Real Estate"]
 for index, kategori in enumerate(kategori_isimleri):
     check_button = Checkbutton(radio_bg, text=kategori, variable=var[index], bg="#DB9C1A", font="Gilroy-bold 16 bold",
                                fg="black")
